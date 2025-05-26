bd441f566ceb0e0e60e79abf4fef0be4730f9825
Richard Quirante <richardquirante98@gmail.com>
2025-05-26 13:38:46 +0800

    r

--- Services/DTO/Report/InvoiceDetailsDTO.cs
+++ Services/DTO/Report/InvoiceDetailsDTO.cs
@@ -69,6 +69,7 @@ namespace EBISX_POS.Services.DTO.Report
                 VatExemptSales = string.Empty,
                 VatSales = string.Empty,
                 VatAmount = string.Empty,
+                PrintCount = string.Empty,
 
                 ElligiblePeopleDiscounts = new List<string>(),
 
